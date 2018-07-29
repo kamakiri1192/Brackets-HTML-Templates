a17c7bae9ceaa69f7152a074720b743378d2147a
kamakiri1192 <kamakiri1192@gmail.com>
2018-07-29 15:35:52 +0900

    日本語訳を追加 add japanese

---
+++ nls/ja/strings.js
@@ -0,0 +1,37 @@
+/*
+* Copyright (c) 2014 Travis Almand. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*/
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, browser: true */
+/*global define, brackets, $ */
+
+define({
+    'MENU_COMMAND': 'HTMLテンプレート...',
+    'TITLE_H1': 'HTMLテンプレート',
+    'NO_FILE_OPEN_ERROR': 'ファイルが開かれていません。 HTMLテンプレートを実行する前にファイルを開いてください。',
+    'TITLE_H4': 'テンプレートを選択してください',
+    'CONTENT_ERROR': '空のファイルではありません。 このまま実行すると内容は上書きされます。',
+    'SECTION_DOCTYPE': 'HTMLテンプレート',
+    'SELECT_LABEL': '選択...',
+    'SECTION_TEMPLATES': 'フレームワーク テンプレート',
+    'SECTION_TEMPLATES_DESC': 'これらのテンプレートは、フレームワークで提供されているHTMLに基づいており、若干の調整が必要です。',
+    'CANCEL_BUTTON': '閉じる'
+});
